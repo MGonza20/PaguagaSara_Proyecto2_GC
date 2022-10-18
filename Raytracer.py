635d9c110c09d1bc16b3c50323528b0b52cd1f3f
MGonza20 <sara.paguaga@gmail.com>
2022-10-17 22:55:56 -0600

    Done

--- Raytracer.py
+++ Raytracer.py
@@ -8,76 +8,52 @@ width = 1024
 height = 1024
 
 # Materiales
-brick = Material(diffuse = (0.8, 0.3, 0.3), spec = 16)
-stone = Material(diffuse = (0.4, 0.4, 0.4), spec = 8)
-wallMat = Material(diffuse = (37/255, 150/255, 190/255), spec = 8)
-white = Material(diffuse = (0.1, 0.1, 0.1), spec = 8)
-marble = Material(spec=64, texture= Texture("marble-tex-example.bmp"), matType= REFLECTIVE)
-
-glass = Material(diffuse = (0.9, 0.9, 0.9), spec = 64, ior = 1.5, matType= TRANSPARENT)
-diamond = Material(diffuse = (0.9, 0.9, 0.9), spec = 64, ior = 2.417, matType= TRANSPARENT)
-mirror = Material(diffuse = (0.9, 0.9, 0.9), spec = 64, matType = REFLECTIVE)
-blueMat = Material(diffuse = (0, 0, 1), spec = 64, matType = OPAQUE)
-marble5 = Material( diffuse = (171/255, 240/255, 1), texture = Texture("colored-tex-8.bmp"), spec = 32,  ior = 1.5, matType = TRANSPARENT) 
-
 table = Material(diffuse = (67/255, 63/255, 60/255), spec = 30, matType = OPAQUE)
 porcelain = Material(diffuse= (218/255, 217/255, 215/255), spec = 80, matType = OPAQUE)
 coffee = Material(diffuse = (176/255, 140/255, 119/255), spec = 64, ior = 1.3354, matType= TRANSPARENT)
 spoon = Material(diffuse = (136/255, 132/255, 131/255), spec = 20, matType= REFLECTIVE)
-neon = Material(diffuse = (6/255,1,1/255), spec = 64, ior = 1.3354, matType= TRANSPARENT)
-white = Material(diffuse = (0.9,0.9), spec = 64, matType = OPAQUE)
 
 rtx = Raytracer(width, height)
+# Enviroment Map
 rtx.envMap = Texture("resources/envMap/brown_photostudio.bmp")
 
+# Lights
 rtx.lights.append( AmbientLight(intensity = 0.5 ))
 rtx.lights.append( DirectionalLight(direction = (-1,-1,-1), intensity = 0.55 ))
-
-# rtx.lights.append( SpotLight(size=5, point = (0, -4, 0), lDir=[0, -1, 0], attenuation=1/2))
 rtx.lights.append( SpotLight(size=25, point = (0, -0.75, 0), attenuation=1, color = (243/255, 238/255, 196/255)))
 
-# rtx.scene.append(Triangle(A = (-0.5-1.5,0+0.5-0.5,-4), B = (1-1.5,1.7+0.5-0.5,-4), C = (0-1.5, 1.5+0.5-0.5, -4), material = marble5))
+# Mesa
 rtx.scene.append( Disk(position = (0,-1.5,-4), radius = 1.5, normal = (0,1,0), material = table ))
 
-# rtx.scene.append( AABB(position = (0,-3,-7), size = (2,2,2), material = marble))
-
-
-# rtx.scene.append(Plane(position = (0, -10, 0), normal = (0, 1, 0), material = wallMat))
-# rtx.scene.append(Plane(position = (0, 20, 0), normal = (0, -1, 0), material = wallMat))
-# rtx.scene.append(Plane(position = (-10, 0, 0), normal = (1, 0, 0), material = wallMat))
-# rtx.scene.append(Plane(position = (10, 0, 0), normal = (-1, 0, 0), material = wallMat))
-# rtx.scene.append(Plane(position = (0, 0, -50), normal = (0, 0, 1), material = porcelain))
-
-# rtx.scene.append(Triangle(A = (-0.5-1.5,0+0.5-0.5,-4), B = (1-1.5,1.7+0.5-0.5,-4), C = (0-1.5, 1.5+0.5-0.5, -4), material = marble5))
-# rtx.scene.append(Triangle(A = (-1*(-0.5-1.5)-0.5 +0.35 ,-1*(0+0.5-1)-0.5-0.25,-4), B = (-1*(1-1.5)-0.5 + 0.35, -1*(1.7+0.5-1)-0.5-0.25,-4), C = (-1*(0-1.5)+0.25 + 0.35, -1*(1.5+0.5-1)-0.5-0.25, -4), material = wallMat))
-# rtx.scene.append(Triangle(A = (-1 -0.1,0-0.5,-3.5), B = (1 -0.1,0-0.5,-3.5), C = (0 -0.1, 1.5-0.5, -3.5), material = wallMat))
-
+# Taza de cafe
 rtx.glLoadModel("resources/models/coffeeCup/coffeeCup6.obj",
                  translate = V3(0, -1.5, -3.5),
                  scale = V3(0.5, 0.5, 0.5),
                  rotate = V3(7,0,0),
                  material = porcelain)
 
+# Plato
 rtx.glLoadModel("resources/models/coffeePlate/coffeePlate3.obj",
                  translate = V3(0, -1.5, -3.5),
                  scale = V3(0.5, 0.5, 0.5),
                  rotate = V3(7,0,0),
                  material = porcelain)
 
-
+# Galleta
 rtx.glLoadModel("resources/models/cookie/cookie.obj",
                  translate = V3(-1.8, -4.3, -2.5),
                  scale = V3(1, 1, 1),
                  rotate = V3(0,40,0),
                  material = Material(texture = Texture("resources/models/cookie/cookieTex.bmp")))
 
+# Cuchara
 rtx.glLoadModel("resources/models/coffeeSpoon/coffeeSpoon9.obj",
                  translate = V3(1.65-0.35, -4.1+0.35, -1.65-0.2),
                  scale = V3(0.8,0.8,0.8),
                  rotate = V3(0,80,0),
                  material = spoon)
 
-
+# Cafe
 rtx.scene.append( Disk(position = (0,-0.7,-3.5), radius = 0.5, normal = (0,1,0), material = coffee ))
 
 
